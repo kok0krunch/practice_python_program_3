8a74fc479b007eb98cf7e1f0e830846d3d68f58a
kok0krunch <amaliakadoi@gmail.com>
2025-03-28 20:27:01 +0800

    Removing leading spaces using loop

--- batch_6/prog01_remove_leading_spaces.py
+++ batch_6/prog01_remove_leading_spaces.py
@@ -2,3 +2,7 @@
 # Create a program that do the same functionality without using lstrip() function.
 
 string = input("Enter a string with leading spaces: ")
+
+# Remove leading spaces using a loop
+while len(string) > 0 and string[0] == ' ':
+    string = string[1:]  # Remove the first character if it's a space
