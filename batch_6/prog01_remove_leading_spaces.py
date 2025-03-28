2e9e144d1f229581423166a0edc03a066445825c
kok0krunch <amaliakadoi@gmail.com>
2025-03-28 20:27:59 +0800

    Display string without leading spaces

--- batch_6/prog01_remove_leading_spaces.py
+++ batch_6/prog01_remove_leading_spaces.py
@@ -6,3 +6,5 @@ string = input("Enter a string with leading spaces: ")
 # Remove leading spaces using a loop
 while len(string) > 0 and string[0] == ' ':
     string = string[1:]  # Remove the first character if it's a space
+
+print(string)
