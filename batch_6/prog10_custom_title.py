e20265ebeae50c921a6212a9adc7ebba3001f9fe
kok0krunch <amaliakadoi@gmail.com>
2025-03-28 22:41:28 +0800

    Create a function to replicate title()

---
+++ batch_6/prog10_custom_title.py
@@ -0,0 +1,11 @@
+# Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. 
+#         Create a program that do the same functionality without using title() function.
+
+# Function to replicate title()
+def custom_title(string):
+    words = string.split()
+    # Capitalize the first letter of each word
+    for i in range(len(words)):
+        words[i] = words[i][0].upper() + words[i][1:].lower()
+    # Join the words back together
+    return ' '.join(words)
