9c31e5afc9791f174019d59149ef0b107d497d52
kok0krunch <amaliakadoi@gmail.com>
2025-03-28 21:07:51 +0800

    Converting uppercase letters to lowercase

--- batch_6/prog03_convert_to_lowercase.py
+++ batch_6/prog03_convert_to_lowercase.py
@@ -3,3 +3,13 @@
 
 # Prompt user to enter a string
 string = input("Enter a string: ")
+
+# Initialize an empty string to store the lowercase version of the input string
+lowercase_string = ""
+
+# Iterate through each character in the input string
+for char in string:
+    if char.isupper(): # Check if the character is uppercase
+        lowercase_string += char.swapcase() # Convert the uppercase character to lowercase and add it to the result string
+    else:
+        lowercase_string += char
