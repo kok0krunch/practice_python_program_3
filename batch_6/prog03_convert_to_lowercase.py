0cf1641d9bd0fab4651622402062cacf87e5e3cb
kok0krunch <amaliakadoi@gmail.com>
2025-03-28 20:49:54 +0800

    Prompt user to input a string

---
+++ batch_6/prog03_convert_to_lowercase.py
@@ -0,0 +1,5 @@
+# Prog03. lower() converts all characters of the string into lower case. 
+#         Create a program that do the same functionality without using lower() function.
+
+# Prompt user to enter a string
+string = input("Enter a string: ")
