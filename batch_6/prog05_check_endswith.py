fdbf841eaaf831d1aca9fde5606af62b7d4d1c39
kok0krunch <amaliakadoi@gmail.com>
2025-03-28 21:38:17 +0800

    Prompt user to input a string and a suffix to check

---
+++ batch_6/prog05_check_endswith.py
@@ -0,0 +1,6 @@
+# Prog05. endswith() check if the string end part matches the function parameter. 
+#         Create a program that do the same functionality without using endswith() function.
+
+# Prompt user to enter a string and a suffix to check
+string = input("Enter a string: ")
+suffix = input("Enter the suffix to check: ")
