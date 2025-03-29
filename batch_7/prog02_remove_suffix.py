5e468141df06cd14d5bbc737b738eedd7f6ced24
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 18:48:54 +0800

    Remove suffix using replace()

--- batch_7/prog02_remove_suffix.py
+++ batch_7/prog02_remove_suffix.py
@@ -4,3 +4,6 @@
 # Prompt user to enter a string and a prefix to remove
 string = input("Enter a string: ")
 suffix = input("Enter the suffix to remove: ")
+
+# Remove the suffix from the string (if it exists at the ending)
+result = string.replace(suffix, "", -1) # Replace the first occurrence of the suffix with an empty string
