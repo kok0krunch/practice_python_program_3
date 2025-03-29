c1058a497910b04e20ac82cbadc646f6c27ccc2f
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 18:48:02 +0800

    Ask user to input a string and a suffix to remove

---
+++ batch_7/prog02_remove_suffix.py
@@ -0,0 +1,6 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. 
+#         Create a program that do the same functionality without using removesuffix() function.
+
+# Prompt user to enter a string and a prefix to remove
+string = input("Enter a string: ")
+suffix = input("Enter the suffix to remove: ")
