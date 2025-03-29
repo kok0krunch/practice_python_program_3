819f2d95b0c042e3384644c85764432d06294d3d
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 18:35:15 +0800

    Ask user to input a string with trailing spaces

---
+++ batch_7/prog01_remove_trailing_space.py
@@ -0,0 +1,5 @@
+# Prog01. rstrip() remove the space characters at the end of the string. 
+#         Create a program that do the same functionality without using rstrip() function.
+
+# Prompt user to input a string with trailing spaces
+string = input("Enter a string with trailing spaces: ")
