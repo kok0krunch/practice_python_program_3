69458846b8ac2e3ca3136996d02e720ed09c107e
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 18:36:32 +0800

    Display result

--- batch_7/prog01_remove_trailing_space.py
+++ batch_7/prog01_remove_trailing_space.py
@@ -7,3 +7,6 @@ string = input("Enter a string with trailing spaces: ")
 # Remove trailing spaces using a loop
 while len(string) > 0 and string[-1] == ' ':  # Check if the last character is a space
         string = string[:-1]  # Remove the last character
+        
+# Print the modified string without trailing spaces
+print(f"String without trailing spaces: '{string}'")       
