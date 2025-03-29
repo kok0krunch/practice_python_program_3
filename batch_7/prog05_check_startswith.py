bcf0ead94b6db63397a30530675d679ffd5a4d78
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 19:24:02 +0800

    Create a function to check if string starts with the prefix

---
+++ batch_7/prog05_check_startswith.py
@@ -0,0 +1,9 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. 
+#         Create a program that do the same functionality without using startswith() function.
+
+# Function to check if a string starts with a specific substring
+def startswith(string, prefix):
+    if string[:len(prefix)] == prefix: # Compare the start of the string with the prefix
+        return True
+    else:
+        return False
