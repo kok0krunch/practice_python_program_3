eb175888b21984bfca471bec216a2d8ed3c7d652
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 18:54:52 +0800

    Ask user to input a string

---
+++ batch_7/prog03_convert_to_uppercase.py
@@ -0,0 +1,5 @@
+# Prog03. upper() converts all characters of the string into upper case. 
+#         Create a program that do the same functionality without using upper() function.
+
+# Prompt user to enter a string
+string = input("Enter a string: ")
