724c7d7235a8adaf01e65f59f27fa7c3ecbf6f77
kok0krunch <amaliakadoi@gmail.com>
2025-03-29 19:02:13 +0800

    Create a function to check if string is in lowercase

---
+++ batch_7/prog04_check_if_lower.py
@@ -0,0 +1,9 @@
+# Prog04. islower() check if all characters of the string is on lower case. 
+#         Create a program that do the same functionality without using islower() function.
+
+# Create a function to check if all characters in a string are lowercase
+def is_lowercase(string):
+    for char in string:
+        if char.isupper():  # Check if the character is uppercase
+            return False
+    return True
